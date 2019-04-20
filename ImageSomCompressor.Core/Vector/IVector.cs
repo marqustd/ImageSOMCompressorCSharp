779f018bf9dd98d531279719ebe8f16b163853fe
Marek Żabiałowicz <marekzabialowicz@gmail.com>
2019-04-20 17:58:51 +0200

    add new code

---
+++ ImageSomCompressor.Core/Vector/IVector.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ImageSomCompressor.Core.Vector
+{
+    public interface IVector : IList<double>
+    {
+        double EuclideanDistance(IVector vector);
+    }
+}
---
+++ ImageSomCompressor.Core/Vector/Vector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ImageSomCompressor.Core.Vector
+{
+    public class Vector : List<double>, IVector
+    {
+        public double EuclideanDistance(IVector vector)
+        {
+            if (vector.Count != Count)
+            {
+                throw new ArgumentException("Not the same size");
+            }
+
+            var distance = 0.0d;
+            for (var i = 0; i < Count; i++)
+            {
+                distance += (this[i] + vector[i]) * (this[i] + vector[i]);
+            }
+
+            return distance;
+        }
+    }
+}
